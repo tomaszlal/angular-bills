9b71a6281df2158b89bdf23c3a2f31ffe8cfa2af
Tomasz Lalewicz <lalewicz.tomasz@gmail.com>
2022-01-08 11:35:57 +0100

    next changes in listbill

--- src/app/listbills/listbills.component.ts
+++ src/app/listbills/listbills.component.ts
@@ -2,6 +2,7 @@ import { Category } from './../model/category';
 import { HttpService } from './../service/http.service';
 import { Component, OnInit } from '@angular/core';
 import { Bill } from '../model/bill';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-listbills',
@@ -13,8 +14,31 @@ export class ListbillsComponent implements OnInit {
   listBills: Array<Bill> = new Array<Bill>();
   listCategories: Array<Category> = new Array<Category>();
 
-  idBillToDelete: number = 0;
-  billInContext?: Bill;
+  idBillInContext: number = 0;
+  billInContext: Bill ={};
+
+  formToEdit: FormGroup = new FormGroup({
+    category: new FormControl(this.listCategories, Validators.required),
+    invoice: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    amount: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d{0,10}(\.\d{1,2})?$/),
+    ]),
+    dateOfIssue: new FormControl('', Validators.required),
+    dateOfPayment: new FormControl('', Validators.required),
+    dueDate: new FormControl(''),
+    wasPaid: new FormControl('')
+  });
+
+
+  public formToPay: FormGroup = new FormGroup({
+    fieldDueDate: new FormControl('',Validators.required)
+  });
+
+
 
   constructor(private httpService:HttpService) { }
 
@@ -27,12 +51,37 @@ export class ListbillsComponent implements OnInit {
 
   public setIdBillToDel(item : Bill){
     console.log(item);
-    this.idBillToDelete = item.id ? item.id : 0;
-    this.httpService.getBillById(this.idBillToDelete).subscribe((bill) =>{
+    this.idBillInContext = item.id ? item.id : 0;
+    this.httpService.getBillById(this.idBillInContext).subscribe((bill) =>{
       this.billInContext = bill;
     });
 
   }
+
+  public setBillToInContext(bill : Bill){
+    this.billInContext = bill;
+  }
+
+  public setBillToEdit(bill: Bill){
+    this.setBillToInContext(bill);
+    this.formToEdit.controls.category.setValue(this.billInContext.category);
+    this.formToEdit.controls.invoice.setValue(this.billInContext.invoiceNumber);
+    this.formToEdit.controls.amount.setValue(this.billInContext.amount);
+    this.formToEdit.controls.dateOfIssue.setValue(this.billInContext.dateOfIssue);
+    this.formToEdit.controls.dateOfPayment.setValue(this.billInContext.dateOfPayment);
+    this.formToEdit.controls.dueDate.setValue(this.billInContext.dueDate);
+    this.formToEdit.controls.wasPaid.setValue(this.billInContext.wasPaid);
+    if (this.billInContext.wasPaid){
+
+      this.formToEdit.controls.dueDate.enable(); // włączenie wyłączenie pola
+      this.formToEdit.controls.wasPaid.enable();
+    } else {
+
+      this.formToEdit.controls.dueDate.disable();
+      this.formToEdit.controls.wasPaid.disable();
+    }
+  }
+
 
 
   public getCategory() {
@@ -59,12 +108,41 @@ export class ListbillsComponent implements OnInit {
     }
   }
 
+  public changeInPaid() {
+
+    this.formToEdit.controls.dueDate.disable();
+    this.formToEdit.controls.wasPaid.disable();
+    this.formToEdit.controls.dueDate.reset();
+  }
+
   public delBillFromDb(){
-    this.httpService.deleteBillById(this.idBillToDelete).subscribe((v) => {
+    this.httpService.deleteBillById(this.idBillInContext).subscribe((v) => {
       this.clearListOfBills();
       this.getBills();
     });
+  }
 
+  public updateBillInDb(){
+    console.log(this.billInContext);
+    this.billInContext.dueDate = this.formToPay.controls.fieldDueDate.value;
+    this.billInContext.wasPaid = true;
+    console.log(this.billInContext);
+    console.log(this.listBills);
+    this.httpService.updateBill(this.billInContext).subscribe((bill) => {
+      console.log(bill);
+    });
+    this.formToPay.reset();
+  }
+
+  public saveBillToDb(){
+    console.log(this.billInContext);
+    console.log(this.formToEdit.value)
 
   }
+
+  //get
+  get getWasPaidField() {
+    return this.formToEdit.get('wasPaid');
+  }
+
 }
--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -4,47 +4,53 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from '../model/category';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  // apiUrl:string = 'http://' + location.hostname + ':8080';
+  apiUrl: string = 'http://192.168.0.210:8080'; // w domu
+  // apiUrl:string = 'http://192.168.192.210:8080'; // poza domem
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getCategory() : Observable <Array<Category>>{
-    return this.http.get<Array<Category>>('http://192.168.0.210:8080/category');
+  getCategory(): Observable<Array<Category>> {
+    return this.http.get<Array<Category>>(this.apiUrl + '/category');
   }
 
-  getCategoryById(id: number): Observable<Category>{
-    return this.http.get<Category>('http://192.168.0.210:8080/category/'+id);
+  getCategoryById(id: number): Observable<Category> {
+    return this.http.get<Category>(this.apiUrl + '/category/' + id);
   }
 
-  addCategory(category: Category): Observable<Category>{
-    return this.http.post<Category>('http://192.168.0.210:8080/categories',category);
+  addCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(this.apiUrl + '/categories', category);
   }
 
-  deleteCategoryById(id: number): Observable<boolean>{
-    return this.http.delete<boolean>(`http://192.168.0.210:8080/category/${id}`);
+  deleteCategoryById(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(this.apiUrl + '/category/' + id);
   }
 
-  checkAccountNumber(accNum: string) : Observable<boolean>{
-    return this.http.get<boolean>('http://192.168.0.210:8080/accountcheck/'+accNum);
+  checkAccountNumber(accNum: string): Observable<boolean> {
+    return this.http.get<boolean>(this.apiUrl + '/accountcheck/' + accNum);
   }
 
   addBill(bill: Bill): Observable<number> {
-    return this.http.post<number>('http://192.168.0.210:8080/bills',bill);
+    return this.http.post<number>(this.apiUrl + '/bills', bill);
   }
 
-  getBills(): Observable<Array<Bill>>{
-    return this.http.get<Array<Bill>>('http://192.168.0.210:8080/bill');
+  getBills(): Observable<Array<Bill>> {
+    return this.http.get<Array<Bill>>(this.apiUrl + '/bill');
   }
 
-  getBillById(id: number): Observable<Bill>{
-    return this.http.get<Bill>('http://192.168.0.210:8080/bill/'+id);
+  getBillById(id: number): Observable<Bill> {
+    return this.http.get<Bill>(this.apiUrl + '/bill/' + id);
   }
 
-  deleteBillById(id: number): Observable<void>{
-    return this.http.delete<void>('http://192.168.0.210:8080/bill/'+id);
-}
+  deleteBillById(id: number): Observable<void> {
+    return this.http.delete<void>(this.apiUrl + '/bill/' + id);
+  }
+
+  updateBill(bill: Bill) : Observable<Bill> {
+    return this.http.put<Bill>(this.apiUrl+'/bills',bill);
+  }
 }
